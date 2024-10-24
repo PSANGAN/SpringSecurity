7d72a7739321ab0b6e2bd4413a64b99539278ca4
PSANGAN <32886735+PSANGAN@users.noreply.github.com>
2024-10-23 22:38:09 -0400

    SecurityIDconnects

--- securitypoc/src/main/java/com/pcgs/spring/securitypoc/EazyBankApiApplication.java
+++ securitypoc/src/main/java/com/pcgs/spring/securitypoc/EazyBankApiApplication.java
@@ -2,16 +2,14 @@ package com.pcgs.spring.securitypoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-
+//@EnableWebSecurity(debug = true)
 /*@EnableJpaRepositories("com.pcgs.spring.securitypoc.repository")
 @EntityScan("com.pcgs.spring.securitypoc.model")*/
-
 public class EazyBankApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EazyBankApiApplication.class, args);
 	}
-
 }
